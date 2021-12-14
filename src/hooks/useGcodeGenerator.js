aa4f8aff09d9dd0ad9e433439f31c95087253369
sam@apostel.be <sam@apostel.be>
2021-12-14 02:35:37 +0100

    implement slicer with three geometry - gets faces - slices - creates gcode outlines and G0 on layer change

--- src/hooks/useGcodeGenerator.js
+++ src/hooks/useGcodeGenerator.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { getSlices } from '../libraries/slicer';
 
 const useGcodeGenerator = () => {
 	const [gcode, setGcode] = useState('');
@@ -6,8 +7,52 @@ const useGcodeGenerator = () => {
 
 	useEffect(() => {
 		if(!geometry) return;
-		setGcode(`G3 X100 R200`);
-		console.log(geometry);
+		const positions = geometry.attributes.position.array
+		const bounds = {};
+		const indexArray = geometry.index?.array ?? [...Array(positions.length / 3)].map((_, i) => i);
+		const faces = [...Array(indexArray.length / 3)].map((_,i) => {
+			const indexes = [...indexArray.slice(i * 3, (i+1) * 3)];
+			const vertices = indexes.map(index => {
+				const vertex = [...positions.slice((index) * 3, (index+1) * 3)];
+				return {
+					x: vertex[0],
+					y: vertex[1],
+					z: vertex[2]
+				};
+			});
+			vertices.forEach(vertex => {
+				if (bounds.bottom === undefined || bounds.bottom > vertex.z) bounds.bottom = vertex.z;
+				if (bounds.top === undefined || bounds.top < vertex.z) bounds.top = vertex.z;
+			});
+			return vertices;
+		})
+
+		const slices = getSlices(
+			{
+				bounds,
+				height: bounds.top - bounds.bottom,
+				faces
+			},
+			{
+				layerHeight: 1
+			}
+		);
+		let fastMove = true;
+		let e=1;
+		const gcodeCommands = slices.flatMap(({ z, edges, shape }) => {
+
+			fastMove = true;
+			return shape.map(({ x, y }) => {
+				if (fastMove) {
+					fastMove = false;
+					return `G0 X${x} Y${y} Z${z}`;
+				}
+				return `G1 X${x} Y${y} Z${z} E${e++}`;
+			});
+
+		});
+		setGcode(gcodeCommands.join(`
+`));
 	}, [geometry]);
 
 	return [gcode, setGcode, geometry, setGeometry];
